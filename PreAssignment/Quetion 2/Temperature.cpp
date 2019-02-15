cdecc32eeadecdd0d65eabc4c524d20c996e7497
Shehanka <hcsperera@outlook.com>
2019-02-15 19:42:45 +0530

    updated

---
+++ PreAssignment/Quetion 2/Temperature.cpp
@@ -0,0 +1,6 @@
+#include "pch.h"
+#include "Temerature.h"
+
+Temperature::Temerature(){
+    
+}
---
+++ PreAssignment/Quetion 2/Temperature.h
@@ -0,0 +1,11 @@
+class Temperature{
+public:
+    Temperature();
+    boolean isEthlyFreezing(int temp);
+    boolean isEthlyBoiling(int temp);
+    boolean isOxygenFreezing(int temp);
+    boolean isOxygenBoiling(int temp);
+    boolean isWaterFreezing(int temp);
+    boolean isWaterBoiling(int temp);
+    ~Temperature();
+}
