fe848e49efc7c7da3dbd515b7ec88c0c63c56383
Shehanka <hcsperera@outlook.com>
2019-02-15 21:18:42 +0530

    completed q2

--- PreAssignment/Quetion 2/Question2.cpp
+++ PreAssignment/Quetion 2/Question2.cpp
@@ -10,7 +10,28 @@ int main(){
     cout << Please enter the temperature : << endl;
     cin >> temp;
 
-    if(temperature->)
+    if(temperature->isEthylFreezing()){
+        cout << "Ethyl will freeze" << endl;
+    }
+    if(temperature->isEthylBoiling()){
+        cout << "Ethyl will boil" << endl;
+    }
+
+    if(temperature->isOxygenFreezing()){
+        cout << "Oxygen will freeze" << endl;       
+    }
+
+    if(temperature->isOxygenBoiling()){
+        cout << "Oxygen will boil" << endl;
+    }
+
+    if(temperature->isWaterFreezing()){
+        cout << "Water will freeze" << endl;
+    }
+
+    if(temperature->isWaterBoiling()){
+        cout << "Water will boil" << endl;
+    }
 
     delete temperature;
 }
