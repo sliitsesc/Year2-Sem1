0b1d26033aac77188457f7baa072bda44feeca45
Shehanka <hcsperera@outlook.com>
2019-02-15 20:39:33 +0530

    edited

--- PreAssignment/Quetion 2/Temperature.cpp
+++ PreAssignment/Quetion 2/Temperature.cpp
@@ -1,6 +1,41 @@
 #include "pch.h"
 #include "Temerature.h"
+#include "Temperature.h"
 
-Temperature::Temerature(){
-    
+Temperature::Temperature()
+{
+}
+
+boolean Temperature::isEthlyFreezing(int temp)
+{
+	return boolean();
+}
+
+boolean Temperature::isEthlyBoiling(int temp)
+{
+	return boolean();
+}
+
+boolean Temperature::isOxygenFreezing(int temp)
+{
+	return boolean();
+}
+
+boolean Temperature::isOxygenBoiling(int temp)
+{
+	return boolean();
+}
+
+boolean Temperature::isWaterFreezing(int temp)
+{
+	return boolean();
+}
+
+boolean Temperature::isWaterBoiling(int temp)
+{
+	return boolean();
+}
+
+Temperature::~Temperature()
+{
 }
