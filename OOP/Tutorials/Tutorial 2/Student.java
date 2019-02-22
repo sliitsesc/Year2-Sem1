5e2b87fb3e6d2579bfcc777baf5a30c17a5a597c
Shehanka <hcsperera@outlook.com>
2019-02-22 15:44:25 +0530

    updated

--- OOP/Tutorials/Tutorial 2/Student.java
+++ OOP/Tutorials/Tutorial 2/Student.java
@@ -5,7 +5,21 @@ public class Student{
     private String studentName;
     private double studentAvg;
 
+    private int mark1, mark2, mark3;
+
+    Student(){
+        this.mark1 = 0;
+        this.mark2 = 0;
+        this.mark3 = 0;
+    }
+
+    private void getAverageMark(){
+        this.studentAvg = (mark1 + mark2 + mark3)/3.0;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input mark 1 : ")
     }
 }
