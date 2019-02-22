dc886391bd0a4682a1dc854acf005181a2f03e71
Shehanka <hcsperera@outlook.com>
2019-02-22 15:35:41 +0530

    java

---
+++ OOP/Tutorials/Tutorial 2/Student.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Student{
+    private String studentId;
+    private String studentName;
+    private double studentAvg;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
