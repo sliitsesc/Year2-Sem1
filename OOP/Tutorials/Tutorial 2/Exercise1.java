d20ea7ffeb00b0c1538625e2c017a908a4ecb523
Shehanka <hcsperera@outlook.com>
2019-02-22 16:05:19 +0530

    bug fixed

--- OOP/Tutorials/Tutorial 2/Exercise1.java
+++ OOP/Tutorials/Tutorial 2/Exercise1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Student{
+class Student{
     private String studentId;
     private String studentName;
     private double studentAvg;
@@ -19,29 +19,6 @@ public class Student{
         this.mark1 = mark1;
         this.mark2 = mark2;
         this.mark3 = mark3;
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Student student = new Student();
-
-        System.out.print("Input Student ID : ");
-        student.setStudentId(scanner.nextLine());
-
-        System.out.println("Input Student Name : ");
-        student.setStudentName(scanner.nextLine());
-
-        System.out.print("Input mark 1 : ");
-        student.setMark1(scanner.nextInt());
-
-        System.out.print("Input mark 2 : ");
-        student.setMark2(scanner.nextInt());
-
-        System.out.print("Input mark 3 : ");
-        student.setMark3(scanner.nextInt());
-
-        student.calAverageMark();
-        student.displayDetails();
     }
 
     public void setStudentId(String studentId) {
@@ -64,14 +41,39 @@ public class Student{
         this.mark3 = mark3;
     }
 
-    private void calAverageMark(){
+    public void calAverageMark(){
         this.studentAvg = (mark1 + mark2 + mark3)/3.0;
     }
 
-    private void displayDetails(){
+    public void displayDetails(){
         System.out.println("Student ID : " + studentId);
         System.out.println("Student Name : " + studentName);
         System.out.println("Student Average : " + studentAvg);
     }
+}
 
+class Exercise1{
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Student student = new Student();
+
+        System.out.print("Input Student ID : ");
+        student.setStudentId(scanner.nextLine());
+
+        System.out.print("Input Student Name : ");
+        student.setStudentName(scanner.nextLine());
+
+        System.out.print("Input mark 1 : ");
+        student.setMark1(scanner.nextInt());
+
+        System.out.print("Input mark 2 : ");
+        student.setMark2(scanner.nextInt());
+
+        System.out.print("Input mark 3 : ");
+        student.setMark3(scanner.nextInt());
+
+        student.calAverageMark();
+        student.displayDetails();
+    }
 }
