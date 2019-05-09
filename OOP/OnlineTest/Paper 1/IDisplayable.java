46126671805cdceedd6a6087c1c7585b5aa4b7da
Chamod Shehanka <hcsperera@outlook.com>
2019-05-09 21:27:00 +0530

    Add files via upload

--- OOP/OnlineTest/Paper 1/IDisplayable.java
+++ OOP/OnlineTest/Paper 1/IDisplayable.java
@@ -1,2 +1,9 @@
-interface IDisplayable{
+/**
+ * @author chamodshehanka on 5/9/2019
+ * @project Demo
+ **/
+public interface IDisplayable {
+    int LEVEL = 6;
+    void display(String msg);
+    String result();
 }
---
+++ OOP/OnlineTest/Paper 1/MyInterface.java
@@ -0,0 +1,6 @@
+/**
+ * @author chamodshehanka on 5/9/2019
+ * @project Demo
+ **/
+public interface MyInterface {
+}
