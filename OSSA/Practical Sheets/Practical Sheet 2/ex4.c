c4862f06269abc2eba685c226848b2a8ce655768
Chamod Shehanka Perera <hcsperera@outlook.com>
2019-05-14 07:09:20 +0530

    ex4

---
+++ OSSA/Practical Sheets/Practical Sheet 2/ex4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(){
+    int ret;
+
+    printf("Hello World\n");
+    ret = fork();
+
+    if(ret == 0)
+        printf("I'm child and return value = %d\n", ret);
+    else
+        printf("I'm parent and return value = %d\n", ret);
+}
