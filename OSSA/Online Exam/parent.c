5784e68fccdbc013bd5aa22ce16c89b37a4aa2d4
Chamod Shehanka Perera <hcsperera@outlook.com>
2019-05-16 12:59:15 +0530

    Bux fixed

--- OSSA/Online Exam/parent.c
+++ OSSA/Online Exam/parent.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 int main()
 {
@@ -15,7 +16,7 @@ int main()
     }
     else
     {
-        wait();
+        wait(NULL);
         printf("Child ID : %d \n", childID);
     }
 }
