b3bd7ff248abbe47101f48b6271082b4f0c1537e
IndudiniThennakoon <thennakoonindudini@gmail.com>
2022-11-06 16:19:44 +0530

    #commit: create Dept table

--- DMS/SQL-Revision.sql
+++ DMS/SQL-Revision.sql
@@ -10,3 +10,14 @@ CREATE TABLE Emp
 	CONSTRAINT emp_chk CHECK (salary > 0)
 );
 
+--Q2--
+
+CREATE TABLE Dept
+(
+	did VARCHAR (20),
+	budger REAL,
+	managerID INTEGER REFERENCES Emp, -- add foreign key - 1st way
+	PRIMARY KEY (did) --add primary key - 2nd way
+);
+
+
