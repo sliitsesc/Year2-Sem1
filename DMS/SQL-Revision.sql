201e700de41f5b613b8915134a3f8f8540094e94
IndudiniThennakoon <thennakoonindudini@gmail.com>
2022-11-07 10:35:27 +0530

    feat(tables) : create tables again

--- DMS/SQL-Revision.sql
+++ DMS/SQL-Revision.sql
@@ -181,3 +181,33 @@ Drop Table Dept
 Drop Table Emp
 
 
+CREATE TABLE Emp
+(
+	eid INTEGER PRIMARY KEY,  --add primary key - 1st way
+	ename VARCHAR(225) NOT NULL,
+	age INTEGER,
+	salary REAL,
+
+	CONSTRAINT emp_chk CHECK (salary > 0)
+);
+
+
+CREATE TABLE Dept
+(
+	did VARCHAR (20),
+	budger REAL,
+	managerID INTEGER REFERENCES Emp, -- add foreign key - 1st way
+	PRIMARY KEY (did) --add primary key - 2nd way
+);
+
+
+CREATE TABLE Works
+(
+	eid INTEGER,
+	did VARCHAR (20),
+	pct_time INTEGER, 
+
+	CONSTRAINT Works_pk PRIMARY KEY (eid,did), -- add primary key - 3rd way
+	CONSTRAINT Works_Emp_fk FOREIGN KEY (eid) REFERENCES Emp, -- add foreign key - 2nd way
+	CONSTRAINT Works_Dept_fk FOREIGN KEY (did) REFERENCES Dept -- add foreign key - 2nd  way
+);
